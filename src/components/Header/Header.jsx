85e84f9617ab8ae55a6d398400764cefcfbeb604
dzhalavian <dzhalavian.danila15@yandex.ru>
2024-10-11 13:59:09 +0300

    initial

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+
+    return (
+        <div className={'header'}>
+            <Button>Закрыть</Button>
+            <span className={'username'}></span>
+        </div>
+    );
+
+};
+
+export default Header;
