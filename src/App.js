aad79e64c5e852e19a5ae0f74c91f97092dcb1c7
dzhalavian <dzhalavian.danila15@yandex.ru>
2024-10-12 16:55:48 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from "./hooks/useTelegram";
 import Header from './components/Header/Header';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 function App() {
