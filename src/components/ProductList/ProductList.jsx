9773131a76c1c1871a1119fc3ae7b6850503a126
dzhalavian <dzhalavian.danila15@yandex.ru>
2024-10-13 03:19:33 +0300

    initial

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -44,7 +44,7 @@ const ProductList = () => {
             },
             body: JSON.stringify(data)
         })
-    }, []);
+    }, [addedItems]);
 
     useEffect( () => {
         tg.onEvent('mainButtonClicked', onSendData)
