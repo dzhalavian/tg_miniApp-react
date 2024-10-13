6b13dce1abb6259e6b14f2222b119489a412b7e7
dzhalavian <dzhalavian.danila15@yandex.ru>
2024-10-13 03:10:48 +0300

    initial

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -27,7 +27,31 @@ const ProductList = () => {
 
     const [addedItems, setAddedItems] = useState([]);
 
-    const {tg} = useTelegram();
+    const {tg, queryId} = useTelegram();
+
+    const onSendData = useCallback( () => {
+        const data = {
+           products: addedItems,
+           totalPrice: getTotalPrice(addedItems),
+            queryId,
+
+
+        }
+        fetch('http://localhost:8000', {
+            method: 'POST',
+            headers: {
+                'COntent-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+    }, []);
+
+    useEffect( () => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        };
+    }, [onSendData]);
 
 
     const onAdd = (product) => {
